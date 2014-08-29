aa55f4c725512cb885cbc9405170aa2a292f403c
cloverhsc <clover.hsc@gmail.com>
2014-08-29 14:34:34 +0800

    1's version
    1's version

---
+++ testWD.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+touch /rooti/nasroot
+declare -i count=0
+echo `date +"%Y %m %d %H:%M"` >>/var/log/wdog.log
+echo "hwdog init start" >>/var/log/wdog.log
+hwdog init
+for ((;;))
+do
+sleep 120
+hwdog clear
+echo `date +"%Y %m %d %H:%M"` >>/var/log/wdog.log
+echo "hwdog clear !" >>/var/log/wdog.log
+
+declare -i mega=`ls /var/log/wdog.log -l |awk '{print $5}'`
+#if log file's content over 1M , will change name.
+if [ "$mega" -gt "1000000" ];then
+        `mv /var/log/wdog.log /var/log/wdog$count.log`
+        count=$(( $count + 1 ))
+fi
+done
